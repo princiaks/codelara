407589b464ab66fa445eb51ceac9fcbdef1935e9
Princia Benagon <111951056+princiabenegon@users.noreply.github.com>
2023-10-08 06:30:05 +0530

    change in register function

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -12,42 +12,27 @@ class UserController extends CI_Controller {
         $this->load->helper('form');
         $this->load->database();
         $this->load->model('user_model');
-        // $this->load->model('admin_model');	 
-		// $this->load->model('site_model');
-		// if ( ! $this->session->userdata('userdata'))
-		// { 
-		// 	// Allow some methods?
-		// 	$allowed = array(
-		// 		'index','user_login'
-		// 	);
-		// 	if ( ! in_array($this->router->fetch_method(), $allowed))
-		// 	{
-		// 		redirect('adminpanel');
-		// 	}
-		// }
+      
      }
 	public function index()
 	{
 		$this->load->view('header')->view('register')->view('footer');
 	}
 	public function register() {
-        // Handle the POST request here
-        // Access POST data using $this->input->post() method
-        $parsed_data = json_decode($_POST, true); 
-    //    $data['name'] = $this->input->post('name');
-    //    $data['email'] = $this->input->post('email');
-    //    $data['password'] = $this->input->post('password');
-       print_r($parsed_data);
-        // Process the registration data as needed
-        // You can save it to the database, perform validation, etc.
-
-        // Example: Saving registration data to a model
-        // $this->load->model('Registration_model');
-       
-        $return=$this->user_model->registerUser($data);
-
-        // Return a response if necessary
-        // echo "Registration successful";
+        $json_data = file_get_contents('php://input');
+        if($json_data)
+        {
+            $data = json_decode($json_data, true);
+            $data['passwordview']=$data['password'];
+            $data['password']=password_hash($data['password'], PASSWORD_BCRYPT);
+            $return=$this->user_model->registerUser($data);
+            echo json_encode(array('result'=>'success'));
+        }
+        else
+        {
+            echo json_encode(array('result'=>'failed'));
+        }
+        // Check if the request contains JSON data
     }
     public function registeruser(){
 
